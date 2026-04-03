0013da09747dd84466a66ef52fafd1794df387a3
newbiejy235 <dbankaii1@gmail.com>
2026-04-03 21:51:55 +0700

    pusing bat dah

--- src/db/database.js
+++ src/db/database.js
@@ -5,6 +5,10 @@ const db = mysql2.createPool({
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
+  port: process.env.DB_PORT || 3306,
+  ssl: {
+    rejectUnauthorized: false, // Memungkinkan koneksi SSL di Railway
+  },
   waitForConnections: true,
   connectionLimit: 10,
 });
